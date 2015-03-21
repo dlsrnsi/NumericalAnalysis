547366ba9be2c2a7f631b98e601a7a831d0be52b
dlsrnsi <dlsrnsladlek@naver.com>
2015-03-22 01:09:54 +0900

    간략화된 분할차분

--- divided_difference.py
+++ divided_difference.py
@@ -1,41 +1,21 @@
 __author__ = 'Ingoo'
 
-def get_div_dif(xList, fxList , resultList = [],n=0):
+def div_dif(xList, fxList ,x,result =0,n=0):
     if len(xList) == n:
-        return resultList
+        print("")
+        print("result :", result)
+        return result
+    elif n==0 :
+        result +=fxList[0]
     n = n+1
-    nextfxList = []
+    nextfxList =[]
     for y in range(len(fxList)-1):
-        result = (fxList[y+1] - fxList[y])/(xList[y+n] - xList[y])
-        print('x%d -x%d : '%(y+n,y),result)
-        resultList.append(result)
-        nextfxList.append(result)
-    return get_div_dif(xList,nextfxList,resultList,n)
-
-def div_dif(xList, fxList,x):
-    resultList = get_div_dif(xList,fxList)
-    result = fxList[0]
-    n = len(fxList)-1
-    pointer = 0
-    while True :
-        if n == 0 :
-            break
-        else :
+        ak = (fxList[y+1] - fxList[y])/(xList[y+n] - xList[y])
+        if y == 0 :
             pi = 1
-            for y in range(len(fxList)-n):
-                pi *= (x-xList[y])
-            print("pi :",pi)
-            print('result :' , resultList[pointer])
-            result += pi*resultList[pointer]
-            pointer += n
-            n -= 1
-    return result
-
-
-
-xList = [1,1.5,2,3,3.5,4]
-fxList = [0,0.4055,0.6931,1.0986,1.2528,1.3863]
-x = 1.75
-
-
-print(div_dif(xList,fxList,x))
+            for  z in range(len(xList) - len(fxList) +1):
+                pi *= (x - xList[z])
+            print( " + (",ak*pi,')',end="")
+            result += ak*pi
+        nextfxList.append(ak)
+    return div_dif(xList,nextfxList,x,result,n)
---
+++ test.py
@@ -0,0 +1,8 @@
+__author__ = 'Ingoo'
+
+from divided_difference import div_dif
+
+xList = [1,1.5,2,3,3.5,4]
+fxList = [0,0.4055,0.6931,1.0986,1.2528,1.3863]
+x = 1.75
+div_dif(xList,fxList,x)
